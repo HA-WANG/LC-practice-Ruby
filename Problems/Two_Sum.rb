1b4894ef2cbd615089d9c6de7c604fbb9a4d4b21
HA-WANG <stone.wwf@gmail.com>
2016-05-09 09:36:59 -0500

    Two Sum

---
+++ Problems/Two_Sum.rb
@@ -0,0 +1,24 @@
+def two_sum(nums, target)
+  map = Hash.new
+  nums.each_with_index do |item, index|
+    diff = map[target - item]
+    return [diff, index] if diff != nil
+    map[item] = index
+  end
+end
+
+nums = [2, 7, 11, 15]
+target = 9
+p two_sum(nums, target)
+
+=begin
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+=end
